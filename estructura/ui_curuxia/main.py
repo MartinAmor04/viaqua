e0150d9b4bebad7886c0b4ccb4284d52e9278079
inesposes <inesposes@gmail.com>
2025-05-20 18:19:49 +0200

    Corrected paths

--- estructura/ui_curuxia/main.py
+++ estructura/ui_curuxia/main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
-from sql_queries import get_data
+from modules.sql_queries import get_alerts, edit_alert
+from modules.audio_conversion import base64_to_audio
 
 st.set_page_config(
     page_title="CuruxIA",  
-    page_icon="./img/favicon.png", 
+    page_icon="./assets/img/favicon.png", 
     layout="centered",  
     initial_sidebar_state="auto"
 )
@@ -24,7 +25,7 @@ def aplicar_css(ruta_css: str):
             {css}
         </style>
     """, unsafe_allow_html=True)
-aplicar_css("styles.css")
+aplicar_css("assets/styles.css")
 
 
 # ID de la fila que se está editando
@@ -38,7 +39,7 @@ col1, col2= st.columns([0.7,0.3])
 with col1:
     st.markdown('<h1 class="titulo-principal">Curux<span>IA</span></h1>', unsafe_allow_html=True)
 with col2:
-    st.image("img/favicon.png", width=150)
+    st.image("assets/img/favicon.png", width=150)
 st.markdown("#### O aparello que supervisa todas as túas máquinas")
 
 
@@ -61,7 +62,7 @@ with st.container():
         st.markdown('<div class="header-row">Acción</div>', unsafe_allow_html=True)
 
 
-    for idx, row in enumerate(get_data()):
+    for idx, row in enumerate(get_alerts()):
         col1, col2, col3, col4, col5, col6 = st.columns([2, 2, 2, 2, 2, 2])
         with col1:
             st.text(row["public_id"])
@@ -70,21 +71,21 @@ with st.container():
         with col3:
             st.text(row["place"])
         with col4:
-            st.audio("./img/sample-3s.wav", format="audio/wav", loop=False)
+            st.audio(base64_to_audio(row["audio_record"]), format="audio/wav", loop=False)
         with col5:
-            if edit_id == row['machine_id']:
-                new_value = st.selectbox("Tipo avería", tipos_averia, key=f"select_{row['machine_id']}")
+            if edit_id == row['id']:
+                new_value = st.selectbox("Tipo avería", tipos_averia, key=f"select_{row['id']}")
             else:
                 st.text(row["alert_type"])
         with col6:
-            if edit_id == row['machine_id']:
-                if st.button("Guardar", key=f"save_{row['machine_id']}"):
-                    st.session_state.alertas.at[idx, "alert_type"] = new_value
+            if edit_id == row['id']:
+                if st.button("Guardar", key=f"save_{row['id']}"):
+                    edit_alert(row['id'], new_value)
                     st.session_state.edit_id = None
                     st.rerun()
             else:
-                if st.button("Editar", key=f"edit_{row['machine_id']}"):
-                    st.session_state.edit_id = row['machine_id']
+                if st.button("Editar", key=f"edit_{row['id']}"):
+                    st.session_state.edit_id = row['id']
                     st.rerun()
 
 st.divider()

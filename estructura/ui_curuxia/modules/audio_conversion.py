a497e6f2e4cda29c98d0d543fcec6f31cf1d8029
inesposes <inesposes@gmail.com>
2025-05-26 20:04:53 +0200

    Subida de volumen de los audios automaticamente

--- estructura/ui_curuxia/modules/audio_conversion.py
+++ estructura/ui_curuxia/modules/audio_conversion.py
@@ -1,5 +1,7 @@
 import base64
-from io import BytesIO  # ¡Aquí está la importación que faltaba!
+from io import BytesIO 
+from pydub import AudioSegment
+
 def audio_to_base64(audio_path):
     with open(audio_path, "rb") as audio_file:
         encoded_audio = base64.b64encode(audio_file.read()).decode("utf-8")
@@ -7,4 +9,13 @@ def audio_to_base64(audio_path):
 
 def base64_to_audio(base64_string):
     audio_bytes = base64.b64decode(base64_string)
-    return BytesIO(audio_bytes) 
+    audio_increased=increase_volume(BytesIO(audio_bytes) )
+    return audio_increased
+
+def increase_volume(audio_low):
+    audio = AudioSegment.from_wav(audio_low)
+    strong_audio = audio + 30
+    output = BytesIO()
+    strong_audio.export(output, format="wav")
+    output.seek(0)
+    return output.getvalue()

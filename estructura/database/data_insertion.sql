ede59adf328cad0ed78dc761f3e45a7cd8b55fcd
inesposes <inesposes@gmail.com>
2025-06-01 20:30:16 +0200

    Concellos correxidos

--- estructura/database/data_insertion.sql
+++ estructura/database/data_insertion.sql
@@ -2,13 +2,13 @@
 ALTER TABLE machine AUTO_INCREMENT = 1;
 
 INSERT INTO machine (public_id, place, machine_type, power) VALUES 
-('SOP001', 'Sistís', 'Soplante', 10),
-('SOP002', 'Santa Cruz 1 ', 'Soplante', 12),
-('SOP003', 'Santa Cruz 2', 'Soplante', 15),
-('SOP004', 'Casar do Mato', 'Soplante', 18),
-('BOM005', 'Sistís', 'Bomba', 9),
-('COM006', 'Santa Cruz 1', 'Compresor', 8),
-('COM007', 'Santa Cruz 2', 'Compresor', 12),
-('GEN008', 'Sistís', 'Motor', 20),
-('MOT009', 'Casar do Mato', 'Motor', 6),
-('BOM010', 'Casar do Mato', 'Bomba', 15);
+('SOP001', 'Dodro', 'Soplante', 10),
+('SOP002', 'Laxe  ', 'Soplante', 12),
+('SOP003', 'Toén', 'Soplante', 15),
+('SOP004', 'Vilaboa', 'Soplante', 18),
+('BOM005', 'Dodro', 'Bomba', 9),
+('COM006', 'Laxe ', 'Compresor', 8),
+('COM007', 'Toén', 'Compresor', 12),
+('GEN008', 'Dodro', 'Motor', 20),
+('MOT009', 'Vilaboa', 'Motor', 6),
+('BOM010', 'Vilaboa', 'Bomba', 15);
